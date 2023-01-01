686564653ff8d75a3250d91a430e8feca3401bbb
pkr187 <pankajroman187@gmail.com>
2023-01-01 14:30:51 +0530

    [pankaj] Add:CreateContact

---
+++ AddressBookProblem/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AddressBookProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book Program");
+            Contact contact = new Contact()
+            {
+                FirstName = "Pankaj",
+                LastName = "kumar",
+                Address = "Banglore",
+                City = "Whitefield",
+                State = "Karnatika",
+                Zip = 560048,
+                PhoneNumber = "8825178849",
+                Email = "pankajroman187@gmail.com"
+            };
+            Console.WriteLine(contact.FirstName + "\n" + contact.LastName + "\n" + contact.Address + "\n" + contact.City + "\n" + contact.State + "\n" + contact.Zip + "\n" + contact.Email);
+        }
+    }
+}
+
+        
+    
